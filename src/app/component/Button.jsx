572e15bdedee812488d04ea5fca9fc821697a28e
fee-hanafee <hanafee.pass@gmail.com>
2024-05-11 18:28:44 +0700

    finish quiz2

--- src/app/component/Button.jsx
+++ src/app/component/Button.jsx
@@ -1,10 +1,13 @@
-export default function Button({ children, onClick ,type}) {
+export default function Button({ children, onClick, type, bg }) {
+  const finalClass =
+    bg == "red"
+      ? "bg-red-500 hover:bg-red-600"
+      : "bg-blue-500 hover:bg-blue-600";
   return (
     <button
-      className="px-3 py-1.5 rounded-md bg-blue-500 hover:bg-blue-600 text-white"
+      className={`px-3 py-1.5 rounded-md ${finalClass} text-white`}
       onClick={onClick}
       type={type}
-      
     >
       {children}
     </button>
--- src/app/quiz2/page.jsx
+++ src/app/quiz2/page.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Input from "../component/Input";
+import Button from "../component/Button";
 
-export default function page() {
+const initialValues = {
+  value1: "",
+  value2: "",
+  value3: "",
+  value4: "",
+  value5: "",
+};
+const percents = {
+  value1: 100,
+  value2: 7,
+  value3: 107,
+  value4: 3,
+  value5: 104,
+};
+
+export default function Page() {
+  const [input, setInput] = useState(initialValues);
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const findValue = (value, percent, percentTarget) => {
+    return (percentTarget / percent) * value;
+  };
+
+  const findIndex = (value) => {
+    let index = "";
+    for (let i in value) {
+      if (value[i] != "") {
+        index = i;
+      }
+    }
+    return index;
+  };
+
+  const calculateValues = () => {
+    try {
+      const index = findIndex(input);
+      for (let i in input) {
+        if (i !== index) {
+          const result = findValue(+input[index], +percents[index], +percents[i]);
+          setInput((prevInput) => ({ ...prevInput, [i]: result }));
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    calculateValues();
+  };
+
+  const onClear = () => setInput(initialValues);
+
   return (
-    <div>page</div>
-  )
+    <div className="p-4">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 bg-gray-100 p-4 rounded-md"
+      >
+        <div className="flex gap-2 text-center">
+          <div>
+            <p>100</p>
+            <Input onChange={handleChange} name="value1" value={input.value1} />
+          </div>
+          <div>
+            <p>7</p>
+            <Input onChange={handleChange} name="value2" value={input.value2} />
+          </div>
+          <div>
+            <p>107</p>
+            <Input onChange={handleChange} name="value3" value={input.value3} />
+          </div>
+          <div>
+            <p>3</p>
+            <Input onChange={handleChange} name="value4" value={input.value4} />
+          </div>
+          <div>
+            <p>104</p>
+            <Input onChange={handleChange} name="value5" value={input.value5} />
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <Button type="submit">Go</Button>
+          <Button bg="red" type="button" onClick={onClear}>
+            Clear
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
